9f2c6d3de4470545770edc5dc34edda03f6c4b32
smile <3499517342@qq.com>
2026-08-27 19:00:03 +0800

    更新项目配置代码

--- InfoApiAuto/config/settings.py
+++ InfoApiAuto/config/settings.py
@@ -1,9 +1,11 @@
 # 公共配置
 import os
+from pathlib import Path
+
 from dotenv import load_dotenv
 
-# 执行 .env 文件加载
-load_dotenv()
+# 执行项目目录中的 .env 文件加载
+load_dotenv(Path(__file__).resolve().parents[1] / ".env")
 
 
 class Settings:
@@ -11,9 +13,10 @@ class Settings:
     LOGIN_PATH = os.getenv("LOGIN_PATH", "")
     NETWORKS_PATH = os.getenv("NETWORKS_PATH", "")
     TOP_TOKENS_PATH = os.getenv("TOP_TOKENS_PATH", "")
+    COLLECTED_TOKENS_PATH = os.getenv("COLLECTED_TOKENS_PATH", "")
     TEST_EMAIL = os.getenv("TEST_EMAIL", "")
     TEST_CODE = os.getenv("TEST_CODE", "")
-    REQUEST_TIMEOUT = os.getenv("REQUEST_TIMEOUT", "15")
+    REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT", "15"))
 
     @classmethod
     def validate(cls):
