ba5e48c29b288e747f565263b0ca50a39d657adb
smile <3499517342@qq.com>
2026-08-31 11:00:08 +0800

    提交收藏功能断言代码

---
+++ InfoApiAuto/asserts/tracking/collected/collected_tokens_assert.py
@@ -0,0 +1,8 @@
+from InfoApiAuto.asserts.common.response_assert import assert_success_response
+
+
+def assert_collect_tokens_success(response):
+    return assert_success_response(response)
+
+def assert_cancel_tokens_success(response):
+    return assert_success_response(response)
